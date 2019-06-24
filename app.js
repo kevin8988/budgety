2bc7d9e4642f3524f4d0803f5af9f26f00e1da6f
KevinChips <kevsilva07@gmail.com>
2019-06-24 12:38:31 -0300

    event listeners

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ var UIController = (function() {
   //Some code later
 })();
 
-var appControlel = (function() {
-  //Some code later
-})();
+var appController = (function(budgetCtrl, UICtrl) {
+  var buttonAdd = ".add__btn";
+  var removeIncomeButton = ".item__delete--btn";
+  var removeExpensesButton = ".";
+
+  document.querySelector(bottomAdd).addEventListener("click", function() {
+    console.log("Add Clicked");
+  });
+
+  document
+    .querySelector(removeIncomeButton)
+    .addEventListener("click", function() {
+      console.log("Remove Income Clicked");
+    });
+})(budgetController, UIController);
