0d24785e2591cb5b5163c1cd6a99a48d2adacac1
KevinChips <kevsilva07@gmail.com>
2019-06-24 12:52:40 -0300

    recipes

--- app.js
+++ app.js
@@ -3,21 +3,18 @@ var budgetController = (function() {
 })();
 
 var UIController = (function() {
-  //Some code later
+  
+
 })();
 
 var appController = (function(budgetCtrl, UICtrl) {
   var buttonAdd = ".add__btn";
-  var removeIncomeButton = ".item__delete--btn";
-  var removeExpensesButton = ".";
 
-  document.querySelector(bottomAdd).addEventListener("click", function() {
-    console.log("Add Clicked");
+  document.querySelector(buttonAdd).addEventListener("click", function() {
+    //1. Get the field input data
+    //2. Add the item to the budget controller
+    //3. Add the item to the UI
+    //4. Calculate the budget
+    //5. Display the budget on the UI
   });
-
-  document
-    .querySelector(removeIncomeButton)
-    .addEventListener("click", function() {
-      console.log("Remove Income Clicked");
-    });
 })(budgetController, UIController);
