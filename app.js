4910233058f4c0a154bc237674ea47e297ff0ca8
KevinChips <kevsilva07@gmail.com>
2019-06-24 15:00:40 -0300

    constructors for inc and exp

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 var budgetController = (function() {
-  //Some code later
+  //Income constructor
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  //Expense constructor
+  var Expense = function(id, description, value, percentage) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+    this.percentage = percentage;
+  };
+
+  //Store data
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
 })();
 
 var UIController = (function() {
+  // Class names
   var DOMStrings = {
     inputType: ".add__type",
     inputDescription: ".add__description",
@@ -10,11 +35,13 @@ var UIController = (function() {
     inputButton: ".add__btn"
   };
 
+  // Get DOM
   var budgetType = document.querySelector(DOMStrings.inputType);
   var budgetDescription = document.querySelector(DOMStrings.inputDescription);
   var budgetValue = document.querySelector(DOMStrings.inputValue);
 
   return {
+    // Return a input values
     getInput: function() {
       return {
         type: budgetType.value,
@@ -23,6 +50,7 @@ var UIController = (function() {
       };
     },
 
+    // Return a class names
     getDOMStrings: function() {
       return DOMStrings;
     }
@@ -30,6 +58,7 @@ var UIController = (function() {
 })();
 
 var appController = (function(budgetCtrl, UICtrl) {
+  // Set up event listeners
   var setUpEventListeners = function() {
     var DOMStrings = UICtrl.getDOMStrings();
     document
@@ -43,17 +72,19 @@ var appController = (function(budgetCtrl, UICtrl) {
     });
   };
 
+  //Add an item
   var appCrtlAddItem = function() {
     //1. Get the field input data
     var input = UICtrl.getInput();
-    console.log(input);
     //2. Add the item to the budget controller
+
     //3. Add the item to the UI
     //4. Calculate the budget
     //5. Display the budget on the UI
   };
 
   return {
+    // Init function
     init: function() {
       setUpEventListeners();
     }
