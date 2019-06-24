4ef094be3b80f3995de498b8a337748c5b2845bf
KevinChips <kevsilva07@gmail.com>
2019-06-24 13:12:05 -0300

    function to add item

--- app.js
+++ app.js
@@ -3,18 +3,28 @@ var budgetController = (function() {
 })();
 
 var UIController = (function() {
-  
-
+  //Some code later
 })();
 
 var appController = (function(budgetCtrl, UICtrl) {
   var buttonAdd = ".add__btn";
 
-  document.querySelector(buttonAdd).addEventListener("click", function() {
+  var appCrtlAddItem = function() {
     //1. Get the field input data
     //2. Add the item to the budget controller
     //3. Add the item to the UI
     //4. Calculate the budget
     //5. Display the budget on the UI
+
+    console.log("was clicked");
+
+  };
+
+  document.querySelector(buttonAdd).addEventListener("click", appCrtlAddItem);
+
+  document.addEventListener("keypress", function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      appCrtlAddItem();
+    }
   });
 })(budgetController, UIController);
